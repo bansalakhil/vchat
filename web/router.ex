fe223f5aa352842c3b03571d3274c0a99ac0af5c
Akhil Bansal <bansalakhil30.10@gmail.com>
2015-12-27 23:31:06 +0530

    Added authentication on home page

--- web/router.ex
+++ web/router.ex
@@ -8,7 +8,10 @@ defmodule Vchat.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :assign_current_user
+  end
 
+  pipeline :auth do
+    plug :authenticate
   end
 
   pipeline :api do
@@ -18,13 +21,21 @@ defmodule Vchat.Router do
   scope "/", Vchat do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    # get "/", PageController, :index
     
-    resources "/users", UserController 
+    resources "/users", UserController, only: [:index, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     
     get "users/activate/:t", UserController, :activate
   end
+
+  scope "/", Vchat do
+    pipe_through [:browser, :auth]  # Use the default browser stack
+
+    get "/", PageController, :index
+    
+  end
+
 
 
   # Fetch the current user from the session and add it to `conn.assigns`. This
@@ -33,6 +44,19 @@ defmodule Vchat.Router do
   defp assign_current_user(conn, _) do
     user_id = get_session(conn, :current_user)
     assign(conn, :current_user, Vchat.UserController.find_by_id(user_id))
+  end
+
+  defp authenticate(conn, _) do
+    current_user_id = get_session(conn, :current_user)
+    current_user = Vchat.UserController.find_by_id(current_user_id)
+    if current_user do
+      assign(conn, :current_user, current_user)
+    else
+      conn
+        |> put_flash(:error, 'You need to be signed in to view this page')
+        |> redirect(to: "/sessions/new")
+        |> halt()
+    end    
   end
 
   # Other scopes may use custom stacks.
